ef778fbd43bb2aa3b916bd849f01915be190c332
Ashutosh Agarwal <ashu.iit11@gmail.com>
2023-07-25 16:32:40 +0530

    class 3

--- App.js
+++ App.js
@@ -1,8 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-let heading = React.createElement("h1",{
-    title:"title"
-},"Heading");
+let Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      alt="Food Villa"
+      src="https://tmlogosave.s3.ap-south-1.amazonaws.com/5653883.jpeg"
+    />
+  </a>
+);
+
+const Header = () => (
+  <div className="header">
+    <Title />
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const Body = () => <h4>Body</h4>;
+
+const Footer = () => <h4>Footer</h4>;
+
+const AppLayout = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<AppLayout />);
